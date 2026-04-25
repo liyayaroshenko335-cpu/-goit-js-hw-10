b7b30a14b16d084d0db97e06dee2104549cbe38b
liyayaroshenko335-cpu <liyayaroshenko335@gmail.com>
2026-04-25 19:47:50 +0300

    error

--- vite.config.js
+++ vite.config.js
@@ -49,8 +49,3 @@ export default defineConfig(({ command }) => {
 
 
 
-export default defineConfig({
-  base: '/goit-js-hw-10/', // Обов'язково замініть на назву ВЛАСНОГО репозиторію
-  // ...інші налаштування (наприклад, injectHTML)
-});
-
